caa62a480c96f037dc1ccee9fce76ddc664e7cce
Dave Blackburn <46696935+dwbconsultingllc@users.noreply.github.com>
2024-10-16 18:10:15 -0700

    update

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -14,6 +14,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "featB": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "featureC": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
